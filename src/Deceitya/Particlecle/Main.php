83e47880612d12d89cd51c908f27072d87b8c893
deceitya <49361292+deceitya@users.noreply.github.com>
2020-04-14 16:35:59 +0900

    更新

--- src/Deceitya/Particlecle/Main.php
+++ src/Deceitya/Particlecle/Main.php
@@ -4,19 +4,17 @@ namespace Deceitya\Particlecle;
 use pocketmine\plugin\PluginBase;
 use pocketmine\level\particle\DustParticle;
 use flowy\Flowy;
-use flowy\standard\DelayTask;
-use flowy\standard\DelayCallbackEvent;
-use function flowy\listen;
+use function flowy\standard\delay;
 
 class Main extends PluginBase
 {
     public function onEnable()
     {
-        require_once($this->getFile() . 'vendor/autoload.php');
+        require_once "{$this->getFile()}vendor/autoload.php";
 
         mt_srand();
 
-        Flowy::run($this, \Closure::fromCallable(function () {
+        Flowy::run($this, function () {
             $original = $this->getServer()->getDefaultLevel()->getSafeSpawn();
             $level = $original->getLevel();
             while (true) {
@@ -27,13 +25,9 @@ class Main extends PluginBase
 
                     $y += 0.04;
 
-                    $task = DelayTask::create();
-                    $this->getScheduler()->scheduleDelayedTask($task, 1);
-                    yield listen(DelayCallbackEvent::class)->filter(function ($ev) use ($task) {
-                        return $ev->getDelayId() === $task->getDelayId();
-                    });
+                    yield from delay($this->getScheduler(), 1);
                 }
             }
-        }));
+        });
     }
 }
